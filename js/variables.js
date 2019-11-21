d8c52261d6e5b139dba36519fd5450d08ebf7ff2
Dusankol <dusank89@gmail.com>
2019-11-21 09:04:58 +0100

    Javascipt variables

---
+++ js/variables.js
@@ -0,0 +1,20 @@
+
+var Peter,
+	Jim,
+	John,
+	firstFlock,
+	secondFlock;
+
+Peter=3500;
+Jim=5000;
+
+console.log(Peter > Jim);
+
+John=38;
+
+console.log("Mike is " + ((John+8)/2-8) + " years old now.");
+
+firstFlock=78;
+secondFlock=57;
+
+console.log(firstFlock-12 >= secondFlock+12-4);
