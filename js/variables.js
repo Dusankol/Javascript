ac2dd7fe73bd3b25265d7c969b6432fa3a2da8bf
Dusankol <dusank89@gmail.com>
2019-11-23 09:59:42 +0100

    Adding variables

--- js/variables.js
+++ js/variables.js
@@ -3,18 +3,29 @@ var Peter,
 	Jim,
 	John,
 	firstFlock,
-	secondFlock;
+	secondFlock,
+	timePeriod,
+	migration;
+
+/*Zadatak 1*/
 
 Peter=3500;
 Jim=5000;
 
 console.log(Peter > Jim);
 
-John=38;
 
-console.log("Mike is " + ((John+8)/2-8) + " years old now.");
+/*Zadatak 2*/
+
+John=38;
+timePeriod=8;
+
+console.log("Mike is " + ((John+timePeriod)/2-timePeriod) + " years old now.");
+
+/*Zadatak 3*/
 
 firstFlock=78;
 secondFlock=57;
+migration=12;
 
-console.log(firstFlock-12 >= secondFlock+12-4);
+console.log(firstFlock-migration >= secondFlock+migration-4);
