a769e3f4b82d9dffd6dbc10011627ca2ad308cde
Dusankol <dusank89@gmail.com>
2020-01-30 01:01:51 +0100

    Domaci iz DOM-a

--- js/main.js
+++ js/main.js
@@ -1,99 +1,254 @@
+var crvenaZvezdaPlayers=[
+{
+  img:"punter.jpg",
+  name:"Kevin",
+  lastName:"Punter",
+  age:26,
+  number:"00",
+  position: "Guard"
+},
+{
+  img:"kuzmic.jpg",
+  name:"Ognjen",
+  lastName:"Kuzmic",
+  age:29,
+  number:0,
+  position: "Center"
+},{
+  img:"dbrown.jpg",
+  name:"Derrick",
+  lastName:"Brown",
+  age:32,
+  number:1,
+  position: "Forward"
+},
+{
+  img:"covic.jpg",
+  name:"Filip",
+  lastName:"Covic",
+  age:30,
+  number:3,
+  position: "Guard"
+},{
+  img:"lbrown.jpg",
+  name:"Lorenzo",
+  lastName:"Brown",
+  age:29,
+  number:4,
+  position: "Guard"
+},{
+  img:"perperoglou.jpg",
+  name:"Stratos",
+  lastName:"Perperoglou",
+  age:35,
+  number:5,
+  position: "Forward"
+},{
+  img:"davidovac.jpg",
+  name:"Dejan",
+  lastName:"Davidovac",
+  age:24,
+  number:7,
+  position: "Forward"
+},{
+  img:"lazic.jpg",
+  name:"Branko",
+  lastName:"Lazic",
+  age:30,
+  number:10,
+  position: "Guard"
+},{
+  img:"baron.jpg",
+  name:"Billy",
+  lastName:"Baron",
+  age:29,
+  number:12,
+  position: "Guard"
+},{
+  img:"dobric.jpg",
+  name:"Ognjen",
+  lastName:"Dobric",
+  age:25,
+  number:13,
+  position: "Guard"
+},{
+  img:"gist.jpg",
+  name:"James",
+  lastName:"Gist",
+  age:33,
+  number:15,
+  position: "Forward"
+},{
+  img:"jagodic-kuridza.jpg",
+  name:"Marko",
+  lastName:"Jagodic-Kuridza",
+  age:32,
+  number:21,
+  position: "Forward"
+},{
+  img:"jenkins.jpg",
+  name:"Charles",
+  lastName:"Jenkins",
+  age:30,
+  number:22,
+  position: "Guard"
+},{
+  img:"simanic.jpg",
+  name:"Borisa",
+  lastName:"Simanic",
+  age:21,
+  number:28,
+  position: "Forward"
+},{
+  img:"jovanovic.jpg",
+  name:"Nikola",
+  lastName:"Jovanovic",
+  age:25,
+  number:32,
+  position: "Center"
+},{
+  img:"ojo.jpg",
+  name:"Michael",
+  lastName:"Ojo",
+  age:26,
+  number:50,
+  position: "Center"
+},{
+  img:"faye.jpg",
+  name:"Mouhammad",
+  lastName:"Faye",
+  age:34,
+  number:11,
+  position: "Forward"
+}
+];
 
+function randomise(arr){
+  return Math.floor(Math.random()*arr.length);
+}
 
-//TASK 1
+function addPlayers(){
+  while(crvenaZvezdaPlayers.length){
 
-function checkBrowserStatus(){
+    var firstSquad=document.querySelector(".firstSquad");
+    var subs=document.querySelector(".subs");
+    var randomNumber=randomise(crvenaZvezdaPlayers)
+    var container= crvenaZvezdaPlayers.length>12? firstSquad:subs;
 
-if (navigator.onLine===true) {
-  alert("Browser is online")
-} else {
-  alert("Browser is offline")
-}};
-
-checkBrowserStatus();
-
-//TASK 2 
-//Zakomentarisao sam, cisto da se stranica ne bi osvezavala sve vreme
-
-/*function refresh(){
-  location.reload()
-};
-
-refresh();*/
-
-
-//TASK 4
-
-var num=0;
-var int;
-
-function count(){
-  console.log(num)
-  num++
-  if(num>15){
-    clearInterval(int)
+    container.appendChild(createPlayer(crvenaZvezdaPlayers[randomNumber]));
+    crvenaZvezdaPlayers.splice(randomNumber,1);
   }
 }
 
-int=setInterval(count,1000)
 
+function createPlayer(playerData){
+  var player=document.createElement("div");
+  player.classList.add("player")
+  var img='<img src="../img/' +playerData.img+ '"/>';
+  var name='<div>'+ playerData.name+" "+playerData.lastName+'</div>';
+  var age='<div>Age: '+ playerData.age+'</div>';
+  var num='<div>Number: '+ playerData.number+'</div>';
+  var position='<div>Position: '+ playerData.position+'</div>';
 
-
-//TASK 5
-
-function randomise(){
-  console.log((Math.round(Math.random()*10)))
-}
-randomise();
-
-
-//TASK 6
-
-// Kod cuvanja u lokalnom skladistu sam objekat prebacio u string da bi mogao da se sacuva, a na kraju sam oznacio korisnika koji je izlogovan kao neaktivan
-
-var result;
-var data=[{name:"Homer",age:36,status:"inactive"},
-          {name:"Marge",age:35,status:"inactive"},
-          {name:"Bart",age:10,status:"inactive"},
-          {name:"Lisa",age:8,status:"inactive"},
-          {name:"Maggie",age:1,status:"inactive"}];
-
-var username=prompt("Enter your name");
-
-
-
-
-var checking=data.some(function(element){
-  if(element.name.toLowerCase().includes(username.toLowerCase())){
-    return true;
-  }
-  else{return false;}
-})
-
-function print(){
-  if(checking===false){
-    console.log("User with name " + username + " doesn't exist.")
-  }
-  else if(checking===true){
-    for(var i=0;i<data.length;i++){
-      if(data[i].name.toLowerCase().includes(username.toLowerCase())){
-        data[i].status="active";
-        localStorage.setItem("LoggedInUser",JSON.stringify(data[i]));
-
-      }
-    }
-  }setTimeout(remove,60000);
+  player.innerHTML=img+name+age+num+position;
+  return player;
 }
 
-print()
+
+function makeSubs(){
+  var firstSquad=document.querySelectorAll(".firstSquad .player");
+  var subs=document.querySelectorAll(".subs .player");
+
+  var firstSquadNumber=randomise(firstSquad);
+  var subsNumber=randomise(subs);
+
+  var firstSquadPlayer=firstSquad[firstSquadNumber];
+  var subsPlayer=subs[subsNumber];
+
+  var subsPrevious=subsPlayer.previousSibling;
+  var subsNext=subsPlayer.nextSibling;
 
 
-function remove(){
-  localStorage.removeItem("LoggedInUser")
-  for(var i=0;i<data.length;i++){
-    if(data[i].name.toLowerCase().includes(username.toLowerCase())){
-      data[i].status="inactive"
-    }
-  }
-  console.log("User " +username+" is logged out.")
-};
+  firstSquadPlayer.after(subsPlayer);
+
+  subsPrevious?subsPrevious.after(firstSquadPlayer):subNext.before(firstSquadPlayer);
+
+  var one=firstSquadPlayer.querySelector("div");
+  var two=subsPlayer.querySelector("div")
+console.log(one.innerHTML+" has been substituted with "+two.innerHTML)
+
+ 
+}
+
+
+addPlayers();
+
+setInterval(makeSubs,4000);
+
+//CSS
+
+//Ne znam da li postoji bolji nacin da se ovo odradi, ali ovo je jedini nacin koji sam nasao, a da radi, obzirom da nacin sa casa ne funkcionise
+
+Object.assign(document.querySelector("body").style,{
+  margin:"0px",
+  padding:"0px",fontFamily:"sans-serif",
+  backgroundColor:"#d0ae6e"});
+Object.assign(document.querySelector("div.main_wrapper").style,{
+  width: "100%",
+  maxWidth: "1200px",
+  margin: "0px auto"});
+//HEADER
+Object.assign(document.querySelector("header").style,{
+  backgroundColor: "#eb1b27",
+  boxShadow: "0px 0px 5px 2px #d0ae6e",
+  padding: "15px"});
+Object.assign(document.querySelector("header a.logo").style,{
+  display:"block"});
+Object.assign(document.querySelector("header a.logo img").style,{
+  position:"relative",
+   left:"80px"});
+//MAIN SECTION
+Object.assign(document.querySelector("h1").style,{
+  textTransform:"uppercase",
+  color: "#eb1c27",
+  marginLeft:"70px"});
+Object.assign(document.querySelector("section.firstSquad").style,{
+  display:"flex",
+  flexWrap:"wrap",
+  justifyContent:"flexStart",
+  marginLeft:"70px"});
+Object.assign(document.querySelector("section.subs").style,{
+  display:"flex",
+  flexWrap:"wrap",
+  justifyContent:"flex-start",
+  paddingTop:"60px",
+  marginTop:"30px",
+  marginLeft:"70px"});
+
+var pl=document.querySelectorAll("div.player");
+for(var i=0;i<pl.length;i++){
+Object.assign(pl[i].style,{width:"18%",marginRight:"20px"})};
+
+
+var plDiv=document.querySelectorAll("div.player div");
+for(var i=0;i<plDiv.length;i++){
+Object.assign(plDiv[i].style,{padding:"10px 0",fontWeight:"bold"})};
+
+var plImg=document.querySelectorAll("div.player img");
+for(var i=0;i<plImg.length;i++){
+Object.assign(plImg[i].style,{width:"80%",marginBottom:"10px"})};
+
+var border= document.createElement("div");
+
+document.querySelector("section.firstSquad").appendChild(border);
+
+
+Object.assign(border.style,{
+  height: "3px",
+  width: "88%",
+  backgroundColor: "#eb1b27",
+  position: "absolute",
+  top: "765px",
+  marginLeft: "70px",
+  right: "95px"});
+
