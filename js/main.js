9ae62a0b07c990b84832ca23f473561cf029ff7a
Dusankol <dusank89@gmail.com>
2020-03-02 03:52:20 +0100

    ES6 sintax

--- js/main.js
+++ js/main.js
@@ -1,112 +1,195 @@
-
-var movies = [
-  {
-    image:"../img/avengers-one.jpg",
-    name:"Avengers"
-  },
-   {
-    image:"../img/avengers-two.jpg",
-    name:"Avengers: Age Of Ultron"
-  },
-   {
-    image:"../img/avengers-three.jpg",
-    name:"Avengers: Infinity Wars"
-  },
-   {
-    image:"../img/avengers-four.jpg",
-    name:"Avengers: Endgame"
-  },
-   {
-    image:"../img/matrix-one.jpg",
-    name:"The Matrix"
-  },
-  {
-    image:"../img/matrix-two.jpg",
-    name:"The Matrix: Reloaded"
-  },
-  {
-    image:"../img/matrix-three.jpg",
-    name:"The Matrix: Revolutions"
-  },
-  {
-    image:"../img/the-lord-of-the-rings-one.jpg",
-    name:"The Lord Of The Rings: The Fellowship Of The Ring"
-  },
-    {
-    image:"../img/the-lord-of-the-rings-two.jpg",
-    name:"The Lord Of The Rings: Two Towers"
-  },
-    {
-    image:"../img/the-lord-of-the-rings-three.jpg",
-    name:"The Lord Of The Rings: The Return Of The King"
-  },
-  {
-    image:"../img/forrest-gump.jpg",
-    name:"Forrest Gump"
-  },
-  {
-    image:"../img/the-shawshank-redemption.jpg",
-    name:"The Shawshank Redemption"
-  }
+const crvenaZvezdaPlayers=[
+{
+  img:"punter.jpg",
+  name:"Kevin",
+  lastName:"Punter",
+  age:26,
+  number:"00",
+  position: "Guard"
+},
+{
+  img:"kuzmic.jpg",
+  name:"Ognjen",
+  lastName:"Kuzmic",
+  age:29,
+  number:0,
+  position: "Center"
+},{
+  img:"dbrown.jpg",
+  name:"Derrick",
+  lastName:"Brown",
+  age:32,
+  number:1,
+  position: "Forward"
+},
+{
+  img:"covic.jpg",
+  name:"Filip",
+  lastName:"Covic",
+  age:30,
+  number:3,
+  position: "Guard"
+},{
+  img:"lbrown.jpg",
+  name:"Lorenzo",
+  lastName:"Brown",
+  age:29,
+  number:4,
+  position: "Guard"
+},{
+  img:"perperoglou.jpg",
+  name:"Stratos",
+  lastName:"Perperoglou",
+  age:35,
+  number:5,
+  position: "Forward"
+},{
+  img:"davidovac.jpg",
+  name:"Dejan",
+  lastName:"Davidovac",
+  age:24,
+  number:7,
+  position: "Forward"
+},{
+  img:"lazic.jpg",
+  name:"Branko",
+  lastName:"Lazic",
+  age:30,
+  number:10,
+  position: "Guard"
+},{
+  img:"baron.jpg",
+  name:"Billy",
+  lastName:"Baron",
+  age:29,
+  number:12,
+  position: "Guard"
+},{
+  img:"dobric.jpg",
+  name:"Ognjen",
+  lastName:"Dobric",
+  age:25,
+  number:13,
+  position: "Guard"
+},{
+  img:"gist.jpg",
+  name:"James",
+  lastName:"Gist",
+  age:33,
+  number:15,
+  position: "Forward"
+},{
+  img:"jagodic-kuridza.jpg",
+  name:"Marko",
+  lastName:"Jagodic-Kuridza",
+  age:32,
+  number:21,
+  position: "Forward"
+},{
+  img:"jenkins.jpg",
+  name:"Charles",
+  lastName:"Jenkins",
+  age:30,
+  number:22,
+  position: "Guard"
+},{
+  img:"simanic.jpg",
+  name:"Borisa",
+  lastName:"Simanic",
+  age:21,
+  number:28,
+  position: "Forward"
+},{
+  img:"jovanovic.jpg",
+  name:"Nikola",
+  lastName:"Jovanovic",
+  age:25,
+  number:32,
+  position: "Center"
+},{
+  img:"ojo.jpg",
+  name:"Michael",
+  lastName:"Ojo",
+  age:26,
+  number:50,
+  position: "Center"
+},{
+  img:"faye.jpg",
+  name:"Mouhammad",
+  lastName:"Faye",
+  age:34,
+  number:11,
+  position: "Forward"
+}
 ];
 
+const randomise=arr=> Math.floor(Math.random()*arr.length);
 
+const addPlayers=()=>{
+  while(crvenaZvezdaPlayers.length){
 
-var input=document.querySelector("#search");
+    let firstSquad=document.querySelector(".firstSquad");
+    let subs=document.querySelector(".subs");
+    let randomNumber=randomise(crvenaZvezdaPlayers)
+    let container= crvenaZvezdaPlayers.length>12? firstSquad:subs;
 
-var movieList=document.querySelector("#movie-list");
-
-function displayMovies(){
-for (var i=0;i<movies.length;i++){
-  var link=document.createElement("a");
-  link.classList.add("movie");
-  movieList.appendChild(link);
-
-  var title=document.createElement("h2");
-  title.innerHTML=movies[i].name;
-  title.classList.add("title");
-  link.appendChild(title);
-
-  var img=document.createElement("img");
-  img.setAttribute("src", movies[i].image);
-  img.classList.add("gallery");
-  link.appendChild(img);
-
-  var remove=document.createElement("h3");
-  remove.innerHTML="X";
-  remove.classList.add("x");
-  link.appendChild(remove);
-}}
-
-displayMovies()
-
-var movieLinks=document.querySelectorAll(".movie");
-var remove=document.querySelectorAll(".x");
-
-function searchMovies() {
-  
-
-  for(var i=0;i<movies.length;i++){
-    var searchTerm=input.value.toUpperCase();
-
-  if(movieLinks[i].text.toUpperCase().indexOf(searchTerm)>-1)
-{
-  movieLinks[i].style.display="block";
-}else{
-  movieLinks[i].style.display="none";
-}
-
-
+    container.appendChild(createPlayer(crvenaZvezdaPlayers[randomNumber]));
+    crvenaZvezdaPlayers.splice(randomNumber,1);
   }
 }
-input.addEventListener("keyup",searchMovies)
-      
 
- /* Ovaj deo oko X koji sklanja odredjeni film nisam nikako mogao ovako kroz petlju da 
-  postavim, stalno mi je pisalo Cannot read property 'style' of undefined
-  at HTMLHeadingElement.Mada je kroz petlju postavljen event listener na svaki od elemenata.
-  Kada se odradi rucno za svaki, funkcionise.
-*/
-for(var i=0;i<remove.length;i++){
-  remove[i].addEventListener("click",function(){movieLinks[i].style.display="none"})
+
+const createPlayer=(playerData)=>{
+  let player=document.createElement("div");
+  player.classList.add("player")
+  let img='<img src="../img/' +playerData.img+ '"/>';
+  let name=`<div>${playerData.name} ${playerData.lastName}</div>`;
+  let age=`<div>Age: ${playerData.age}</div>`;
+  let num=`<div>Number: ${playerData.number}</div>`;
+  let position=`<div>Position:  ${playerData.position}</div>`;
+
+
+  player.innerHTML=img+name+age+num+position;
+  return player;
 }
+
+
+const makeSubs=()=>{
+  let firstSquad=document.querySelectorAll(".firstSquad .player");
+  let subs=document.querySelectorAll(".subs .player");
+
+  let firstSquadNumber=randomise(firstSquad);
+  let subsNumber=randomise(subs);
+
+  let firstSquadPlayer=firstSquad[firstSquadNumber];
+  let subsPlayer=subs[subsNumber];
+
+  let subsPrevious=subsPlayer.previousSibling;
+  let subsNext=subsPlayer.nextSibling;
+
+
+  firstSquadPlayer.after(subsPlayer);
+
+  subsPrevious?subsPrevious.after(firstSquadPlayer):subNext.before(firstSquadPlayer);
+
+  let one=firstSquadPlayer.querySelector("div");
+  let two=subsPlayer.querySelector("div")
+console.log(`${one.innerHTML} has been substituted with ${two.innerHTML}`)
+
+ 
+}
+
+
+addPlayers();
+
+setInterval(makeSubs,4000);
+
+
+const border= document.createElement("div");
+
+border.classList.add("red")
+
+document.querySelector("section.firstSquad").appendChild(border);
+
+
+
